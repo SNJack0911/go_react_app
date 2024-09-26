31fcaa04e48f82f6e079a5880d4b4d5e3d6fcd31
NGUYEN DUC HAI <duc-hai.nguyen@univ-tlse3.fr>
2024-09-26 17:01:55 +0200

    nice

--- go_side/main.go
+++ go_side/main.go
@@ -7,14 +7,35 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	Id        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello,  World!")
 
 	app := fiber.New()
 
-	log.Fatal(app.Listen(":4000"))
-
+	todos := []Todo{}
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
 	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{} // Create a new instance {id:0,body:"",completed:false}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
+		}
+		todo.Id = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+	log.Fatal(app.Listen(":4000"))
 }
